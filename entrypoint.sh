2580f398ca4c983f23012c4922038be5909f9526
Danny van der Jagt <dannyvanderjagt@gmail.com>
2020-04-21 17:04:51 +0200

    Add NETLIFY_SITE_PROD_ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,24 +4,21 @@ npm i -g netlify-cli
 
 NETLIFY_AUTH_TOKEN=$1
 NETLIFY_SITE_ID=$2
+NETLIFY_SITE_PROD_ID=$6
 NETLIFY_DEPLOY_TO_PROD=$3
 BUILD_DIRECTORY=$4
 FUNCTIONS_DIRECTORY=$5
 
-# Install dependencies
-npm i
-
-# Build project
-npm run build
 
 # Export token to use with netlify's cli
-export NETLIFY_SITE_ID=$NETLIFY_SITE_ID
 export NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN
 
 # Deploy with netlify
 if [ -z $NETLIFY_DEPLOY_TO_PROD ]
 then
+  export NETLIFY_SITE_ID=$NETLIFY_SITE_ID
 	netlify deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message="$INPUT_NETLIFY_DEPLOY_MESSAGE"
 else
+  export NETLIFY_SITE_ID=$NETLIFY_SITE_PROD_ID
 	netlify deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --prod --message="$INPUT_NETLIFY_DEPLOY_MESSAGE"
 fi
